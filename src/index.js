ebb56c7adb8967b96fd61c761493c98d6c6ebc8c
seankrummel <sean.krummel@gmail.com>
2018-12-21 10:28:52 -0600

    rendered the React component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import CheeseList from './components/cheese-list';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<CheeseList cheeses={["Bath Blue", "Barkham Blue", "Buxton Blue"]} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
